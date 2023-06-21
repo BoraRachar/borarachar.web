02c3ce90c8ae9a1cbec7de060463cf0c6f0c59ef
Alysson Chiarelly <alyssonclr@gmail.com>
2023-06-21 12:24:54 -0300

    feat:The DropDown component has been refactored

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -20,23 +20,28 @@ export default function DropDown({ title, message }: DropDownProps) {
   const dropdownHeight = activeButton ? 'auto' : '89px';
 
   return (
-    <div className={`flex flex-col w-full min-h-${dropdownHeight} border-b mb-4 border-[#A7A7A7]`}>
-      <div 
-        className='flex justify-between text-xl font-color-dark mb-4' 
-        id={`dropdown-${title}`}
-      >
-        {title}
-        <button onClick={(event) => handleClickButton(event)} id="dropdown-button">
-          {!activeButton && <Image src={plusIcon} alt="" width={30} height={30} />}
-          {activeButton && <Image src={minusIcon} alt="" width={30} height={30} />}
-        </button>
-      </div>
-      <div 
-        className={`text-justify font-color-dark mb-5 overflow-hidden ${activeButton ? '' : 'line-clamp-2'}`}
-        id={`dropdown-${message}`}  
-      >
-        <p>{message}</p>
-      </div>
-    </div>
+      <details className={`flex flex-col w-full min-h-${dropdownHeight} border-b mb-4 border-[#A7A7A7]`}>
+        <summary className='list-none'>
+          <div 
+            className='flex justify-between text-xl font-color-dark mb-4'
+            id={`dropdown-${title}`}
+          >
+            {title}
+            <button onClick={(event) => handleClickButton(event)} id={`dropdown-button-${title}`}>
+              {!activeButton ? (
+                <Image src={plusIcon} alt="" width={30} height={30} />
+              ) : (
+                <Image src={minusIcon} alt="" width={30} height={30} />
+              )}
+            </button>
+          </div>
+          <div 
+            className={`text-justify font-color-dark mb-5 overflow-hidden ${activeButton ? '' : 'line-clamp-2'}`}
+            id={`dropdown-${message}`}  
+          >
+            <p>{activeButton && message}</p>
+          </div>
+        </summary>
+      </details>
   );
 }
