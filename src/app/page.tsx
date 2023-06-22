b43d88e0f3f3bf91c2a830f8b6e15d93250fc5d8
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2023-06-22 08:07:29 -0300

    feat: creating services section and mount the carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-"use client";
-import Buttons from "@/components/Button";
-import Card from "@/components/Card";
-import Carousel from "@/components/Carousel";
 import Header from "@/components/Header";
 import HeroSection from "@/sections/HeroSection";
+import ServicesSection from "@/sections/ServicesSection";
 
 export const metadata = {
-   title: "Bora Rachar | Home",
+   title: "Bora Rachar | Página inicial",
    description: "...",
 };
 
@@ -37,68 +34,12 @@ const secondaryCardsList = [
    },
 ];
 
-const primaryCardsList = [
-   {
-      title: "Cursos",
-      description: "Netflix, Spotify, HBO Max, Disney+ e muito mais!",
-      url: "#",
-   },
-   {
-      title: "Cursos",
-      description: "Netflix, Spotify, HBO Max, Disney+ e muito mais!",
-      url: "#",
-   },
-   {
-      title: "Cursos",
-      description: "Netflix, Spotify, HBO Max, Disney+ e muito mais!",
-      url: "#",
-   },
-   {
-      title: "Cursos",
-      description: "Netflix, Spotify, HBO Max, Disney+ e muito mais!",
-      url: "#",
-   },
-   {
-      title: "Cursos",
-      description: "Netflix, Spotify, HBO Max, Disney+ e muito mais!",
-      url: "#",
-   },
-   {
-      title: "Cursos",
-      description: "Netflix, Spotify, HBO Max, Disney+ e muito mais!",
-      url: "#",
-   },
-];
-
 export default function Home() {
    return (
       <main>
          <Header />
-       <HeroSection />
-         <Carousel>
-            {primaryCardsList.map((card, index) => (
-               <Card
-                  key={`${card.title}-${index}`}
-                  type="primary"
-                  id={`card-${card.title}-${index}`}
-               >
-                  <div
-                     className={`flex flex-col justify-between min-h-[472px]`}
-                     style={{ width: "calc(100vw / 4 - 85px)" }}
-                  >
-                     <div className="flex flex-col gap-8">
-                        <h3 className="text-3xl font-medium text-white">
-                           {card.title}
-                        </h3>
-                        <p className="mb-32 text-xl font-normal text-white">
-                           {card.description}
-                        </p>
-                     </div>
-                     <Buttons type="secondary" title="Ver grupos" />
-                  </div>
-               </Card>
-            ))}
-         </Carousel>
+         <HeroSection />
+         <ServicesSection />
       </main>
    );
 }
