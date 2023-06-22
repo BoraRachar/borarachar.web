f7444e730a9d12be08286bb7b34c5e0451f6256a
Bitter zinho <thiago.dorville@souunit.com.br>
2023-06-22 14:08:49 -0300

    remove props from newsletter component

--- src/components/Footer/Newsletter.tsx
+++ src/components/Footer/Newsletter.tsx
@@ -1,49 +1,39 @@
 import Email from "@/assets/Email.svg";
 import Image from "next/image";
 
-interface NewsletterProps {
-  title: string;
-  description: string;
-  placeholder: string;
-  bottom_text: string;
-}
-
-export default function Newsletter({
-  title,
-  description,
-  placeholder,
-  bottom_text,
-}: NewsletterProps) {
+export default function Newsletter() {
   return (
     <section>
-      <h1 className="font-semibold text-lg" id={`newsletter-title-${title}`}>
-        {title}
+      <h1 className="font-semibold text-lg" id={`newsletter-title`}>
+        Subscribe to Newsletter
       </h1>
       <p
         className="text-[#637381] text-[15px] mt-7 w-[270px] h-12"
-        id={`newsletter-description-${description}`}
+        id={`newsletter-description`}
       >
-        {description}
+        Enter your email address for recieving valuable newsletters.
       </p>
-      <fieldset className="mt-6 flex" id={`newsletter-input-wrapper-${title}`}>
+      <fieldset className="mt-6 flex" id={`newsletter-input-wrapper`}>
         <input
           className="placeholder-[#637381] w-[222px] text-sm flex pl-5 outline-none border-[#EEEEEE] border-[1px] rounded-l-[5px]"
-          id={`newsletter-input-${title}`}
-          placeholder={placeholder}
+          id={`newsletter-input`}
+          placeholder="Enter your email address"
         />
         <figure
           className="bg-primary w-12 h-12 flex items-center justify-center rounded-r-[5px]"
-          id={`newsletter-icon-wrapper-${title}`}
+          id={`newsletter-icon-wrapper`}
         >
           <Image
             className=""
-            id={`newsletter-icon-${title}`}
+            id={`newsletter-icon`}
             src={Email}
             alt="Icone de email"
           />
         </figure>
       </fieldset>
-      <p className="mt-[25px] text-[#637381] text-[15px]">{bottom_text}</p>
+      <p className="mt-[25px] text-[#637381] text-[15px]">
+        © Copyright 2023 - TailGrids
+      </p>
     </section>
   );
 }
