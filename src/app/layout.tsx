6694d562372cf637ec9292417a1fd60da1f6aa4e
Alysson Chiarelly <alyssonclr@gmail.com>
2023-07-07 20:13:45 -0300

    feat: added roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-"use client";
+'use client';
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
-const queryClient = new QueryClient();
+const roboto = Roboto({ 
+   weight: ["400", "500", "700"],
+   subsets: ["latin"], 
+});
 
-// export const metadata = {
-//   title: 'Bora Rachar',
-//   description: '...',
-// }
+const queryClient = new QueryClient();
 
 export default function RootLayout({
    children,
@@ -23,7 +22,7 @@ export default function RootLayout({
       <html lang="pt-br">
          <Provider store={store}>
             <QueryClientProvider client={queryClient}>
-               <body className={inter.className}>
+               <body className={roboto.className}>
                   <Header />
                   {children}
                </body>
