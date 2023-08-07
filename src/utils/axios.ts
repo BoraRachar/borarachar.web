1def44a656ee4b49e18e325847a59f1dd60935a5
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2023-08-07 12:57:13 -0300

    refactor: adding base url and exporting the instace of Axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const instance = axios.create({
-    baseURL: '',
-    timeout: 0,
-    headers: {}
+export const axiosClient = axios.create({
+   baseURL: "http://borarachar.online:8080/v1",
+   timeout: 0,
+   headers: {
+      "Content-type": "application/json",
+   },
 });
